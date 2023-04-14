120270d5a1b14f99669bfcda25c3233dd9b34d4b
Dmitri Kuznetsov <kna_int@mail.ru>
2023-04-14 22:28:59 +0300

    Added basic example

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace ConsoleApp
+{
+    /// <summary>
+    /// This example is based on - https://learn.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/source-generators-overview
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+
+        static partial void HelloFrom(string name);
+    }
+}
---
+++ ConsoleAppGenerator/Class1.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace ConsoleAppGenerator
+{
+    public class Class1
+    {
+
+    }
+}
