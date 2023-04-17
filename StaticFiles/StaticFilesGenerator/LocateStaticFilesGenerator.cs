54d943e31e61c9f5fc20ac96f3019d1eabf5c8cc
Dmitri Kuznetsov <kna_int@mail.ru>
2023-04-17 22:10:14 +0300

    Added incremental generator

--- StaticFiles/StaticFilesGenerator/LocateStaticFilesGenerator.cs
+++ StaticFiles/StaticFilesGenerator/LocateStaticFilesGenerator.cs
@@ -1,9 +1,30 @@
-﻿using System;
+﻿using Microsoft.CodeAnalysis;
+using System.IO;
 
 namespace StaticFilesGenerator
 {
-    public class Class1
+    [Generator]
+    public class LocateStaticFilesGenerator : IIncrementalGenerator
     {
+        public void Initialize(IncrementalGeneratorInitializationContext initContext)
+        {
+            // define the execution pipeline here via a series of transformations:
 
+            // find all additional files that end with .txt
+            IncrementalValuesProvider<AdditionalText> textFiles = initContext.AdditionalTextsProvider.Where(static file => file.Path.EndsWith(".txt"));
+
+            // read their contents and save their name
+            IncrementalValuesProvider<(string name, string content)> namesAndContents = textFiles.Select((text, cancellationToken) => (name: Path.GetFileNameWithoutExtension(text.Path), content: text.GetText(cancellationToken)!.ToString()));
+
+            // generate a class that contains their values as const strings
+            initContext.RegisterSourceOutput(namesAndContents, (spc, nameAndContent) =>
+            {
+                spc.AddSource($"ConstStrings.{nameAndContent.name}", $@"
+    public static partial class ConstStrings
+    {{
+        public const string {nameAndContent.name} = ""{nameAndContent.content}"";
+    }}");
+            });
+        }
     }
 }
