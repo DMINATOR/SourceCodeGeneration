f856fc3bf69719a65a0d60c4ce13a02f495b554e
Dmitri Kuznetsov <kna_int@mail.ru>
2023-05-03 23:09:45 +0300

    Initial example

--- AdvancedExample/Advanced.App/Program.cs
+++ AdvancedExample/Advanced.App/Program.cs
@@ -1,12 +1,23 @@
-﻿namespace StaticFilesApp
+﻿using System.Xml.Linq;
+
+namespace StaticFilesApp
 {
+    /// <summary>
+    /// This example is based on - https://learn.microsoft.com/en-us/dotnet/csharp/roslyn-sdk/source-generators-overview
+    /// </summary>
     partial class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Console App !");
+            HelloFromSourceGenerator("Console App !");
 
+            HelloFromIncrementalGenerator("Console App !");
         }
+
+        // This will be generated
+        static partial void HelloFromSourceGenerator(string name);
+
+        static partial void HelloFromIncrementalGenerator(string name);
     }
 }
 
